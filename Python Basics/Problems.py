6313cc1ee87c6359362c7d58bd173d5bf123b0e3
Gaurang Parante <gaurangparante2299@gmail.com>
2026-02-02 14:59:17 +0530

    Conditional statement problem solve

---
+++ Python Basics/Problems.py
@@ -0,0 +1,75 @@
+# Write a program to check if number is positive.
+
+# num = int(input("Enter a number: "))
+# print("Its positive") if num > 0 else print("Its negetive")
+
+
+# Write a program to check number is odd or even
+
+# num = int(input("Enter number here: "))
+# if num % 2 == 0 :
+#     print("Its even number")
+# else:
+#     print("Its ODD number")
+
+
+# write a program to create area calculator 
+
+# print("***** AREA CALCULATOR *****")
+# print("""
+# press 1 to get area of square
+# press 2 to get area of rectangle
+# press 3 to get area of circle
+# press 4 to get area of triangle
+# """)
+
+# choice = int(input("Enter a number between 1 to 4: "))
+
+# if choice == 1:
+#     side = float(input("Enter the length of one side: "))
+#     area = side ** 2
+#     print("The area of Square is ",area)
+# elif choice == 2:
+#     length = float(input("Enter the length of rectangle: "))
+#     width = float(input("Enter the width of rectangle: "))
+#     area = length * width
+#     print("The area of Rectangle is ",area)
+# elif choice == 3:
+#     radius = float(input("Enter the radius of the circle: "))
+#     area = ((22/7) * (radius ** 2))
+#     print("The area of Circle is ",area)
+# elif choice == 4:
+#     base = float(input("Enter the base of the triangle: "))
+#     height = float(input("Enter the height of the triangle: "))
+#     area = 0.5 * (base * height)
+#     print("The area of Triangle is ",area)
+
+# else:
+#     print("invalid input")
+
+
+# Write a program check whether the passed latter is vowel or not
+
+# letter = input("Enter a letter here: ")
+
+# if (letter in "aeiou") or (letter in "AEIOU"):
+#     print("It is a vowel.")
+# else:
+#     print("It is not a vowel")
+
+
+# Write a program to check if a number is a singal digit number 
+# 2 - digit number and so on ..., up to 5 digits.
+
+# num = int(input("Enter a number here up to 5 digits: "))
+
+# if num >= 0 and num <= 9:
+#     print("It is a single digit number")
+# elif num >= 10 and num <= 99:
+#     print("It is double digit number.")
+# elif num >= 100 and num <= 999:
+#     print("It is triple digit number.")
+# elif num >= 1000 and num <= 9999:
+#     print("It is four digit number.")
+# else:
+#     print("It is five digit number.")

d91495d08b8340f8403294598bae20f81585d177
Gaurang Parante <gaurangparante2299@gmail.com>
2026-02-03 12:10:22 +0530

    Some other problem of pattern

--- Python Pattern/Problems.py
+++ Python Pattern/Problems.py
@@ -50,3 +50,49 @@
 #         print("*",end=" ")
 #     print()
     
+# 1
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
+# Write a program to display this pattern
+# for i in range(1,6):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+# * 
+# * * 
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * * 
+# * 
+# Write a program to display this pattern
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+# for i in range(4,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# 1 
+# 2 4 
+# 3 6 9
+# 4 8 12 16
+# 5 10 15 20 25
+# 6 12 18 24 30 36
+# 7 14 21 28 35 42 49
+# 8 16 24 32 40 48 56 64
+# write a program to display the following pattern
+
+# for i in range(1,9):
+#     for j in range(1,i+1):
+#         print(i * j,end=" ")
+#     print()

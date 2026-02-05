4f067642c41b1028481f302a77214839979ab743
Gaurang Parante <gaurangparante2299@gmail.com>
2026-02-05 18:29:36 +0530

    Learn

---
+++ Python Strings/Problems2.py
@@ -0,0 +1,33 @@
+# Take input from user as string then, reverse it.
+# a = input("Enter something here: ")
+# print(a[::-1])
+
+# Write a program to check if string contains only digit,
+# a = input("Enter something here: ")
+# print(a.isdigit())
+
+# Wrire a program to check if a string is palindrome.
+# a = input("Enter something here: ")
+# temp = a
+# rev = a[::-1]
+# if temp == rev:
+#     print("This string is palindrome.")
+# else:
+#     print("This string is not palindrome.")
+
+# Write a program to find number of vowels in a string
+# a = input("Enter something here: ")
+# vowels = 0
+# for i in a:
+#     if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U' :
+#         vowels += 1
+# print("Count of vowels in string are",vowels)
+
+# Write a program to check if every word in a string begains with a capital letter.
+
+# a = input("Enter something here: ")
+# title = a.istitle()
+# if title == True:
+#     print("Yes every word in a string begains with a capital letter")
+# else:
+#     print("No every word in a string is not begains with a capital letter")
---
+++ test.py
@@ -0,0 +1,7 @@
+employees = {
+    1:{"name":"gaurang","age":27,"gender":"male"},
+    2:{"name":"bhavesh","age":23,"gender":"male"},
+    3:{"name":"dhaval","age":32,"gender":"male"}
+    }
+# print(employees)
+# print(employees[2])

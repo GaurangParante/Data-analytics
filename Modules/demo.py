581f4a58b176dd80b564b3bf39c92395c2cf2f11
Gaurang Parante <gaurangparante2299@gmail.com>
2026-04-01 15:31:38 +0530

    Modules

---
+++ Modules/demo.py
@@ -0,0 +1,4 @@
+def add(x,y):
+    return x+y
+
+employee = {"name":"gaurang","age":27,"gender":"male"}
---
+++ Modules/main.py
@@ -0,0 +1,10 @@
+import demo as d
+
+a = d.add(12,13)
+print(a)
+
+b = d.employee['name']
+print(b)
+
+c = d.employee['age']
+print(c)
